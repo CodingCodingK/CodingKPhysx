d2c74782279cf11e22285ae1f4004e8c93ffa7d4
CodingCodingK <2470939431@qq.com>
2022-03-09 00:10:03 +0800

    0308

--- UnityDemo/Assets/Scripts/Physx/CodingK_CylinderCollider.cs
+++ UnityDemo/Assets/Scripts/Physx/CodingK_CylinderCollider.cs
@@ -43,7 +43,33 @@ namespace CodingKPhysx
 
         public override bool DetectBoxContact(CodingK_BoxCollider col, ref CodingKVector3 normal, ref CodingKVector3 borderAdjust)
         {
-            throw new System.NotImplementedException();
+            CodingKVector3 disOffset = mPos - col.mPos;
+            CodingKInt dot_disX = CodingKVector3.Dot(disOffset, col.mDir[0]);
+            CodingKInt dot_disZ = CodingKVector3.Dot(disOffset, col.mDir[2]);
+            
+            // 投影钳制在x轴向量
+            CodingKInt clamp_x = CodingKCalc.Clamp(dot_disX, -col.mSize.x, col.mSize.x);
+            CodingKInt clamp_z = CodingKCalc.Clamp(dot_disZ, -col.mSize.z, col.mSize.z);
+            // 计算轴向上的投影向量
+            CodingKVector3 s_x = clamp_x * col.mDir[0];
+            CodingKVector3 s_z = clamp_z * col.mDir[2];
+            // 计算表面最近的接触点：碰撞体中心位置 + 轴向偏移
+            CodingKVector3 point = col.mPos;
+            point += s_x;
+            point += s_z;
+
+            CodingKVector3 po = mPos - point;
+            po.y = 0;
+            if (po.sqrMagnitude > mRadius * mRadius)
+            {
+                return false;
+            }
+            else
+            {
+                normal = po.normalized; // 法线向量
+                CodingKInt len = po.magnitude;
+                borderAdjust = normal * (mRadius - len);
+            }
         }
     }
 }
